ba310b3cbbba3edaa04ed2cbed8edcba7f83d857
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2021-02-11 22:28:56 -0500

    add topBar component #8

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <Footer />
+    <top-bar />
+    <!-- <Footer /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Footer from './Footer.vue'
+// import Footer from './Footer.vue'
+import TopBar from "./TopBar.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Footer
+    TopBar
   }
-}
+};
 </script>
