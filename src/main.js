9582a95fd2380ebb68e60993b2351624c6e17e48
danielhuamani <danielhuamani15@gmail.com>
2021-02-11 20:52:49 -0500

    add styles of bulma

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import '@/assets/styles/_main.scss'
+
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
