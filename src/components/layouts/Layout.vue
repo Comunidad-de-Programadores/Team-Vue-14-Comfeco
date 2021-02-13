67f80fb16b9d5c98ace58a65f58e24bd93118da2
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2021-02-12 21:21:37 -0500

    layout for forms

---
+++ src/components/layouts/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <header>
+        <top-bar />
+      </header>
+      <main>
+          <slot></slot>
+      </main>
+      <footer>
+      <Footer />
+      </footer>
+    </div>
+</template>
+<script>
+import TopBar from "/src/components/TopBar/TopBar.vue";
+import Footer from "/src/components/Footer/Footer.vue";
+export default {
+  name: "Layout",
+  components: {
+    TopBar,
+    Footer
+  }
+};
+</script>
