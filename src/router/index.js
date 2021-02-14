b83f981faee19b6f97aa53e0afc2ee608cb76c23
danielhuamani <danielhuamani15@gmail.com>
2021-02-14 17:27:05 -0500

    add pages

--- src/router/index.js
+++ src/router/index.js
@@ -48,6 +48,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
+  {
+    path: '/recuperar-cuenta',
+    name: 'RecoverPassword',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/RecoverPassword.vue')
   }
 ]
 
---
+++ src/views/RecoverPassword.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <layout>
+      <container-small>
+        <recover-password />
+      </container-small>
+    </layout>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Layout from "@/components/layouts/Layout";
+import ContainerSmall from "@/components/ContainerSmall"
+import RecoverPassword from '@/components/Form/RecoverPassword'
+export default {
+  name: "Home",
+  components: {
+    Layout,
+    ContainerSmall,
+    RecoverPassword
+  }
+};
+</script>
