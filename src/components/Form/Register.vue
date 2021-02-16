c81ffa66349ddfe9fb84b697d927097d3e59db1e
Andresrom1123 <amclres@gmail.com>
2021-02-16 14:00:31 -0600

    add vuedilate for the register

--- src/components/Form/Register.vue
+++ src/components/Form/Register.vue
@@ -1,44 +1,190 @@
 <template>
   <div
-    class="container my-4 is-family-sans-serif"
+    class="container my-4"
     style="width: 90%; max-width: 500px"
   >
     <container-form>
-      <form class="">
+      <form @submit.prevent="onSubmit">
         <div class="pb-5 pt-4 has-text-centered">
-          <a class="is-size-6 px-5" style="border-right: 1px solid;" href="#">
-            Iniciar sesión
-          </a>
-          <a class="is-size-6 px-5" href="#">Registrate</a>
+          <p class="is-size-4 px-5">Registrate</p>
         </div>
 
-        <b-field>
-          <b-input placeholder="Nombre de usuario"></b-input>
-        </b-field>
-        <b-field>
-          <b-input placeholder="Correo" type="email"></b-input>
-        </b-field>
-        <b-field>
-          <b-input placeholder="Contraseña" type="password"></b-input>
-        </b-field>
-        <b-field>
-          <b-input placeholder="Confirmar contraseña" type="password"></b-input>
-        </b-field>
-
-        <div class="buttons">
-          <b-button type="is-primary" expanded>Confirmar</b-button>
+        <div class="mb-3">
+          <b-field
+            :type="$v.form.username.$error ? 'is-danger' : ''"
+            class="mb-0"
+          >
+            <b-input
+              v-model.trim="$v.form.username.$model"
+              placeholder="Nombre de usuario"
+              type="text"
+            >
+            </b-input>
+          </b-field>
+          <div>
+            <span
+              v-if="!$v.form.username.required && $v.form.username.$error"
+              class="is-size-7 is-block"
+              style="color: red"
+            >
+              El nombre de usuario es requerido.
+            </span>
+            <span
+              v-if="!$v.form.username.minLength && $v.form.username.$error"
+              class="is-size-7 is-block"
+              style="color: red"
+            >
+              El nombre de usuario debe tener al menos {{$v.form.username.$params.minLength.min}} letras.
+            </span>
+            <span
+              v-if="!$v.form.username.maxLength && $v.form.username.$error"
+              class="is-size-7"
+              style="color: red"
+            >
+              El nombre de usuario no debe tener más de {{$v.form.username.$params.maxLength.max}} letras.
+            </span>
+          </div>      
         </div>
+
+        <div class="mb-3">
+          <b-field
+            :type="$v.form.email.$error ? 'is-danger' : ''"
+            class="mb-0"
+          >
+            <b-input
+              v-model.trim="$v.form.email.$model"
+              placeholder="Correo"
+              type="email"
+            >
+            </b-input>
+          </b-field>
+          <div>
+            <span
+              v-if="!$v.form.email.required && $v.form.email.$error"
+              class="is-size-7"
+              style="color: red"
+            >
+              El correo es requerido.
+            </span>
+          </div>   
+        </div> 
+
+        <div class="mb-3">
+          <b-field
+            :type="$v.form.password.$error ? 'is-danger' : ''"
+            class="mb-0"
+          >
+            <b-input
+              v-model.trim="$v.form.password.$model"
+              placeholder="Contraseña"
+              type="password"
+            >
+            </b-input>
+          </b-field>
+           <div>
+            <span
+              v-if="!$v.form.password.required && $v.form.password.$error"
+              class="is-size-7 is-block"
+              style="color: red"
+            >
+              La contraseña es requerida.
+            </span>
+          </div>
+          <span
+              v-if="!$v.form.password.minLength && $v.form.password.$error"
+              class="is-size-7"
+              style="color: red"
+            >
+              La contraseña debe tener al menos {{$v.form.password.$params.minLength.min}} letras.
+            </span>
+        </div>
+
+        <div class="mb-3">
+          <b-field
+            :type="$v.form.repeatPassword.$error ? 'is-danger' : ''"
+            class="mb-0"
+          >
+            <b-input
+              v-model.trim="$v.form.repeatPassword.$model"
+              placeholder="Confirmar contraseña"
+              type="password"
+            >
+            </b-input>
+          </b-field>
+          <div>
+            <span
+              v-if="!$v.form.repeatPassword.required && $v.form.repeatPassword.$error"
+              class="is-size-7"
+              style="color: red"
+            >
+              El confirmar contraseña es requerida.
+            </span>
+            <span
+              v-if="!$v.form.repeatPassword.sameAsPassword"
+              class="is-size-7 is-block"
+              style="color: red"
+            >
+              Las contraseñas deben ser idénticas.
+            </span>
+          </div>
+        </div>
+
+        <p class="my-5">
+          ¿Ya tienes una cuenta?
+          <router-link to="/">inicia sesión</router-link>
+        </p>
+
+        <button type="submit" class="button is-dark is-medium is-fullwidth">
+          Confirmar
+        </button>
 
       </form>
     </container-form>
   </div>
 </template>
 <script>
+import { required, minLength, maxLength, email, sameAs } from 'vuelidate/lib/validators'
 import ContainerForm from '@/components/Form/ContainerForm'
 export default {
   name: 'Register',
   components:{
     ContainerForm
+  },
+  data() {
+    return {
+      form: {
+        username: '',
+        email: '',
+        password: null,
+        repeatPassword: null
+      }
+    }
+  },
+  validations: {
+    form: {
+      username: {
+        required,
+        minLength: minLength(4),
+        maxLength: maxLength(20)
+      },
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(5)
+      },
+      repeatPassword: {
+        required,
+        sameAsPassword: sameAs('password')
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$v.$touch()
+    }
   }
 }
 </script>
