e97e2d0278461c4b712825a40e36b74e09315f04
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2021-02-16 22:37:13 -0500

    dashboard template

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <layout>
+       <welcome-view />
+      </layout>
+    </div>
+</template>
+<script>
+import Layout from "@/components/layouts/Layout";
+import WelcomeView from "./Welcome";
+
+export default {
+    name:"Dashboard",
+    components:{
+        Layout,
+        WelcomeView
+        }
+}
+</script>
