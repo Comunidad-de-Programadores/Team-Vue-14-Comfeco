0986a37799b50a51d52cf9f55c36378c635c0154
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2021-02-16 22:36:51 -0500

    route for dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -50,6 +50,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
   },
   {
+    path: '/dashboard',
+    name: 'Welcome',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+  },
+  {
     path: '/recuperar-cuenta',
     name: 'RecoverPassword',
     // route level code-splitting
