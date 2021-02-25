a30d1cbaa16450f5819a7277e9106807abb30ddb
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2021-02-25 15:20:09 -0500

    #23 sponsorsViews

--- src/components/Community/index.vue
+++ src/components/Community/index.vue
@@ -4,17 +4,19 @@
     <p class="is-size-4 has-text-centered	mt-1">¡Conoce gente, aprende y gana!</p>
     <p class="is-size-6	 mt-5">La próxima adición regresa en el 2022, en la cual se planea involucrar a todos los programadores independiente del area de conocimiento que se encuentre, todo con un mismo proposito, aprender en comunidad</p>
     <community-carrousel></community-carrousel>
+    <sponsor-views/>
   </div>
 </template>
 <script>
 import CommunityCarrousel from './CommunityCarrousel'
+import SponsorViews from '@/views/Sponsor.vue'
 export default {
   name: 'Comunity',
-  components:{
-    CommunityCarrousel
+  components: {
+    CommunityCarrousel,
+    SponsorViews
   }
 }
 </script>
 <style lang="scss">
-  
 </style>
