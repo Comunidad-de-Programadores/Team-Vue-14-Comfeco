81c4ea193fc234831524b316905e9d505ed111a2
Diego Saravia Mamani <63614444+DiegoXD93@users.noreply.github.com>
2021-02-25 16:55:03 -0400

    community list item

---
+++ src/components/layouts/LayoutDefault.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header-menu />
+    <main class="container">
+      <div class="columns">
+        <div class="column is-one-quarter">
+          <slot name="left-sidebar"></slot>
+        </div>
+        <div class="column is-two-quarter">
+          <slot></slot>
+        </div>
+        <div class="column is-one-quarter">
+          <slot name="right-sidebar"></slot>
+        </div>
+      </div>
+    </main>
+    <Footer />
+  </div>
+</template>
+<script>
+import HeaderMenu from '@/components/HeaderMenu'
+import Footer from '@/components/Footer'
+export default {
+  name: 'Layout',
+  components: {
+    HeaderMenu,
+    Footer,
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/styles/_main.scss';
+.container {
+  margin-top: 10px;
+}
+</style>
