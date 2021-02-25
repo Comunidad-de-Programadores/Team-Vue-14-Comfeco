e90958616a4aad3b4de289ff4a0ddb7a54a35622
Diego Saravia Mamani <63614444+DiegoXD93@users.noreply.github.com>
2021-02-25 16:55:03 -0400

    community list item

---
+++ src/components/CommunitiesListCard/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <article class="card">
+    <div class="card-content">
+      <div class="header">
+        <span>Comunidades</span>
+        <a href="#">Ver mas</a>
+      </div>
+      <community-list-item
+        v-for="(community, index) in communities"
+        :key="index"
+        v-bind="community"
+      ></community-list-item>
+    </div>
+  </article>
+</template>
+
+<script>
+import CommunityListItem from '@/components/CommunitiesListCard/CommunityListItem'
+export default {
+  components: {
+    CommunityListItem,
+  },
+  data() {
+    return {
+      communities: [
+        {
+          img:
+            'http://www.paredro.com/wp-content/uploads/2015/11/LOGOS-CUADRADOS-05.png',
+          name: 'Comunidad de programadores',
+          link: '#',
+        },
+        {
+          img:
+            'http://www.paredro.com/wp-content/uploads/2015/11/LOGOS-CUADRADOS-05.png',
+          name: 'El lenguaje de los programadores ',
+          link: '#',
+        },
+        {
+          img:
+            'http://www.paredro.com/wp-content/uploads/2015/11/LOGOS-CUADRADOS-05.png',
+          name: 'Latam Dev',
+          link: '#',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_main.scss';
+.card {
+  background-color: $purple-light-2;
+  margin-top: 10rem;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 8px;
+  font-size: 0.8rem;
+  span {
+    color: white;
+  }
+}
+</style>
