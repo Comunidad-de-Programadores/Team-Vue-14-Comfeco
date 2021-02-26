c34a6f65a2d7a2740eeb5c7d7b82c89cd4f5db6c
danielhuamani <danielhuamani15@gmail.com>
2021-02-25 20:45:49 -0500

    add dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,6 +4,10 @@
         <div class="container-dashboard-center">
           <community />
           <sponsor-views/>
+          <div class="">
+            <h3 class="is-size-5 mt-4 has-text-weight-semibold	has-text-centered mb-2">Preperate lo bueno esta por venir</h3>
+            <Counter></Counter>
+          </div>
        </div>
       </layout>
     </div>
@@ -11,13 +15,15 @@
 <script>
 import Layout from "@/components/layouts/Layout";
 import Community from '@/components/Community/index';
+import Counter from '@/components/Counter';
 import SponsorViews from '@/views/Sponsor.vue'
 export default {
   name:"Dashboard",
   components:{
     Layout,
     Community,
-    SponsorViews
+    SponsorViews,
+    Counter
   }
 }
 </script>
